ef5122670e19b9baa62d5b6efdcf1a39de36f86e
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-08-27 15:45:49 -0300

    Criação do modelo Reserva

---
+++ Hotelaria/src/main/java/com/dolts/hotelaria/models/ReservaModel.java
@@ -0,0 +1,38 @@
+package com.dolts.hotelaria.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "reservas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class ReservaModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime dataCriacaoReserva;
+    private LocalDateTime dataAlteracaoReserva;
+    private LocalDateTime dataCancelamento;
+    private LocalDate dataEntrada;
+    private LocalDate dataSaida;
+    private Boolean checkedIn;
+    private Boolean checkedOut;
+    private Boolean cancelada;
+    private String motivoCancelamento;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HospedeModel> hospedes;
+    @OneToMany
+    private List<QuartoModel> quarto;
+    private BigDecimal valorEstadia;
+}
