e663c1f3cfb0fd931f8a4cd262f48b523e4dc1c1
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-08-31 21:42:36 -0300

    Novos componentes

---
+++ hotelaria-frontend/src/app/shared/components/base-crud/base-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'base-crud',
+  templateUrl: './base-crud.component.html',
+  styleUrls: ['./base-crud.component.scss']
+})
+export class BaseCrudComponent {
+
+}
