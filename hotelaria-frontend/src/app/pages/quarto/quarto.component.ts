5439d40edfc8b911569a99af192e16d69400e983
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-08-31 22:53:43 -0300

    Implementação da estrutura base do menu e adição da fonte awesome

---
+++ hotelaria-frontend/src/app/pages/quarto/quarto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quarto',
+  templateUrl: './quarto.component.html',
+  styleUrls: ['./quarto.component.scss']
+})
+export class QuartoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
