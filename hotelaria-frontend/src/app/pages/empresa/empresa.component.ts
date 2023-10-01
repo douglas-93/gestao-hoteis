93b684f8fc488a9d227f5703128603a1fc2af538
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-10-01 19:54:40 -0300

    Criando os campos do cadastro de empresas.

--- hotelaria-frontend/src/app/pages/empresa/empresa.component.ts
+++ hotelaria-frontend/src/app/pages/empresa/empresa.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ModeEnum} from "../../shared/enums/mode.enum";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-empresa',
@@ -6,11 +8,34 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./empresa.component.scss']
 })
 export class EmpresaComponent implements OnInit {
+    mode: ModeEnum = ModeEnum.LIST;
 
-    constructor() {
+    constructor(private router: Router) {
+        let edit: boolean = this.router.url.includes('edit');
+        this.mode = (this.router.url.includes('cad') ||
+            this.router.url.includes('edit')) ? ModeEnum.EDIT : ModeEnum.LIST;
+
+        if (edit) {
+            this.findEmpresa(this.router.url.split('/').pop()!)
+        }
     }
 
     ngOnInit(): void {
     }
 
+    buscar() {
+
+    }
+
+    novo() {
+        this.router.navigate(['empresas', 'cad'])
+    }
+
+    salvar() {
+
+    }
+
+    findEmpresa(id: string) {
+
+    }
 }
---
+++ hotelaria-frontend/src/app/shared/models/empresa.model.ts
@@ -0,0 +1,9 @@
+export class EmpresaModel {
+    razaoSocial: string;
+    nomeFantasia: string;
+    cnpj: string;
+    inscricaoEstadual: string;
+    ramoAtividade: string;
+    observacao: string;
+    ativo: boolean;
+}
