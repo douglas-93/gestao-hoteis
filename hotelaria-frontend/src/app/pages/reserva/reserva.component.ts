4d1e3dd05a97d3708c16f495e6815000b3a9e7a5
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-10-03 19:51:25 -0300

    .

--- hotelaria-frontend/src/app/pages/reserva/reserva.component.ts
+++ hotelaria-frontend/src/app/pages/reserva/reserva.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ModeEnum} from "../../shared/enums/mode.enum";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-reserva',
@@ -6,11 +8,34 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./reserva.component.scss']
 })
 export class ReservaComponent implements OnInit {
+    mode: ModeEnum = ModeEnum.LIST;
 
-    constructor() {
+    constructor(private router: Router) {
+        let edit: boolean = this.router.url.includes('edit');
+        this.mode = (this.router.url.includes('cad') ||
+            this.router.url.includes('edit')) ? ModeEnum.EDIT : ModeEnum.LIST;
+
+        if (edit) {
+            this.findReserva(this.router.url.split('/').pop()!)
+        }
     }
 
     ngOnInit(): void {
     }
 
+    findReserva(id: string) {
+
+    }
+
+    buscar() {
+
+    }
+
+    novo() {
+        this.router.navigate(['hospedes', 'cad'])
+    }
+
+    salvar() {
+
+    }
 }
