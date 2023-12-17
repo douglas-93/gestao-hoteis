8cabfd19bb601762dcebed12e076f5c69ddc5d93
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-12-17 16:04:15 -0300

    Formatar tamanho de arquivo.

--- hotelaria-frontend/src/app/shared/Utils.ts
+++ hotelaria-frontend/src/app/shared/Utils.ts
@@ -57,4 +57,21 @@ export class Utils {
         });
         return formatador.format(valor);
     }
+
+    static formatFileSize(sizeInBytes: number | undefined): string {
+        if (sizeInBytes != undefined) {
+            const kilobyte = 1024;
+            const megabyte = kilobyte * 1024;
+
+            if (sizeInBytes < kilobyte) {
+                return sizeInBytes + ' Bytes';
+            } else if (sizeInBytes < megabyte) {
+                return (sizeInBytes / kilobyte).toFixed(2) + ' KB';
+            } else {
+                return (sizeInBytes / megabyte).toFixed(2) + ' MB';
+            }
+        }
+
+        return '';
+    }
 }
