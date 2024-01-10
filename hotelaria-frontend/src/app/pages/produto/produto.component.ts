b70b462aa3666d5d09e03d2f59164b4a26192427
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2024-01-09 23:48:48 -0300

    Inicio do cadastro de produto

--- hotelaria-frontend/src/app/pages/produto/produto.component.ts
+++ hotelaria-frontend/src/app/pages/produto/produto.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {ProdutoModel} from "../../shared/models/ProdutoModel";
 import {TiposProdutoEnum} from "../../shared/enums/TiposProdutoEnum";
-import {DxSelectBoxComponent} from "devextreme-angular";
+import {DxSelectBoxComponent, DxTextBoxComponent} from "devextreme-angular";
+import {ModeEnum} from "../../shared/enums/mode.enum";
+import _ from "lodash";
+import notify from "devextreme/ui/notify";
 
 @Component({
-  selector: 'app-produto',
-  templateUrl: './produto.component.html',
-  styleUrls: ['./produto.component.scss']
+    selector: 'app-produto',
+    templateUrl: './produto.component.html',
+    styleUrls: ['./produto.component.scss']
 })
 export class ProdutoComponent implements OnInit {
 
@@ -14,13 +17,52 @@ export class ProdutoComponent implements OnInit {
 
     produto: ProdutoModel;
     tiposProdutoEnumSelectBox;
+    mode: ModeEnum;
 
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-      this.produto = new ProdutoModel();
-      this.tiposProdutoEnumSelectBox = Object.keys(TiposProdutoEnum).map(key => ({ value: key, displayText: TiposProdutoEnum[key].getDescricao() }));
-  }
+    ngOnInit(): void {
+        this.produto = new ProdutoModel();
+        this.tiposProdutoEnumSelectBox = Object.keys(TiposProdutoEnum).map(key => ({
+            value: key
+          , displayText: TiposProdutoEnum[key].getDescricao()
+        }));
+    }
 
+    validaCampos() {
+        if (_.isEmpty(this.produto.nome) || _.isNil(this.produto.nome)) {
+            notify('O nome do produto é obrigatório', 'error', 3600);
+            return false;
+        }
+
+        if (_.isNil(this.produto.valor)) {
+            notify('O valor do produto é obrigatório', 'error', 3600);
+            return false;
+        }
+
+        if (_.isEmpty(this.produto.tipo) || _.isNil(this.produto.tipo)) {
+            notify('O tipo do produto é obrigatório', 'error', 3600);
+            return false;
+        }
+        return true;
+    }
+
+    protected readonly ModeEnum = ModeEnum;
+
+    salvar() {
+        if (this.validaCampos()) {
+            console.log(this.produto)
+            notify('Vamo salvar', 'success', 3600);
+        }
+    }
+
+    defineValor(e) {
+        this.produto.valor = e.value;
+    }
+
+    defineTipo(e) {
+        this.produto.tipo = e.value
+    }
 }
