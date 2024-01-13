1cab1ea6338a84ad2a2bce4b8684da33cba83e8a
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2024-01-13 17:30:51 -0300

    Criação do serviço para buscar o número da sequência

---
+++ hotelaria-frontend/src/app/shared/services/sequenciador.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from "@angular/core";
+import {BaseCRUDService} from "./base-crud.service";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import notify from "devextreme/ui/notify";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SequenciadorService extends BaseCRUDService<number> {
+
+    constructor(http: HttpClient) {
+        super('/sequenciador', http);
+    }
+
+    public proximoNumero(atributo: string) {
+        const params = new HttpParams()
+            .set('atributo', atributo);
+
+        return this.http.get<number>(`${this.url}/proximo`, {params, observe: 'response'});
+    }
+}
