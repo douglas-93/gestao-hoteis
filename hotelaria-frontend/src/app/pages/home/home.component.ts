2c91e69b231ff8b4e03d8b967b40ed734117e3a2
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2024-01-19 23:17:34 -0300

    Inicio de criação da tela inicial

--- hotelaria-frontend/src/app/pages/home/home.component.ts
+++ hotelaria-frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
 import {Component} from '@angular/core';
+import {HotelService} from "../../shared/services/hotel.service";
+import {HotelModel} from "../../shared/models/hotel.model";
+import {ArquivoDIgitalModel} from "../../shared/models/ArquivoDIgitalModel";
+import {ArquivoDigitalService} from "../../shared/services/arquivoDigitalService";
+import {lastValueFrom} from "rxjs";
 
 @Component({
     templateUrl: 'home.component.html',
@@ -6,6 +11,32 @@ import {Component} from '@angular/core';
 })
 
 export class HomeComponent {
-    constructor() {
+
+    hotel: HotelModel;
+    arquivoDigital: ArquivoDIgitalModel;
+    dataHoraHoje: number;
+
+    constructor(private hotelService: HotelService,
+                private arquivoDigitalService: ArquivoDigitalService) {
+        this.dataHoraHoje = Date.now();
+    }
+
+    async ngOnInit() {
+        this.hotel = new HotelModel();
+        setInterval(() => this.atualizaDataHora(), 1000);
+        let resp = await lastValueFrom(this.hotelService.findById(3));
+        if (resp.ok) {
+            this.hotel = resp.body!;
+        }
+
+        let response = await lastValueFrom(this.arquivoDigitalService.findById(this.hotel.logoMarcaId));
+        if (response.ok) {
+            this.arquivoDigital = response.body!;
+            this.hotel.logoAsDataSource = `data:${this.arquivoDigital.tipo};base64,${this.arquivoDigital.dados}`;
+        }
+    }
+
+    atualizaDataHora() {
+        this.dataHoraHoje = Date.now();
     }
 }
