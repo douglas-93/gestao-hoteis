d5441781549907cedda0f32322a47e386d596981
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2024-01-19 21:44:39 -0300

    Ajustes de menus

--- hotelaria-frontend/src/app/app-navigation.ts
+++ hotelaria-frontend/src/app/app-navigation.ts
@@ -4,94 +4,70 @@ export const navigation = [
         path: '/home',
         icon: 'home'
     },
-    // {
-    //   text: 'Examples',
-    //   icon: 'folder',
-    //   items: [
-    //     {
-    //       text: 'Profile',
-    //       path: '/profile'
-    //     },
-    //     {
-    //       text: 'Tasks',
-    //       path: '/tasks'
-    //     }
-    //   ]
-    // },
     {
         text: 'Hotel',
         path: '/hotel',
         icon: 'fa-solid fa-building'
     },
     {
-        text: 'Cadastro de Quarto',
+        text: 'Cadastro de Quartos',
         icon: 'fa-solid fa-door-closed',
         items: [
             {
-                text: 'Categoria',
+                text: 'Categorias',
                 path: '/categorias',
                 icon: 'fa-solid fa-gem'
             },
             {
-                text: 'Tipo',
+                text: 'Tipos',
                 path: '/tipos',
                 icon: 'fa-solid fa-people-group'
             },
             {
-                text: 'Quarto',
+                text: 'Quartos',
                 path: '/quartos',
                 icon: 'fa-solid fa-door-open'
             }
         ]
     },
     {
-        text: 'Hospede',
+        text: 'Hospedes',
         path: '/hospedes',
         icon: 'fa-solid fa-user'
     },
     {
-        text: 'Empresa',
+        text: 'Empresas',
         path: '/empresas',
         icon: 'fa-solid fa-city'
     },
     {
-        text: 'Reserva',
+        text: 'Monitor de Reservas',
+        path: '/monitor-reservas',
+        icon: 'fa-solid fa-table'
+    },
+    {
+        text: 'Reservas',
         path: '/reservas',
-        icon: 'fa-solid fa-calendar'
+        icon: 'fa-solid fa-calendar-days'
+    },
+    {
+        text: 'Consumo de Reservas',
+        path: '/consumo',
+        icon: 'fa-solid fa-cart-shopping'
+    },
+    {
+        text: 'Cadastro Produtos',
+        path: '/produto',
+        icon: 'fa-solid fa-barcode'
+    },
+    {
+        text: 'Movimentos',
+        path: '/estoque',
+        icon: 'fa-solid fa-arrow-right-arrow-left'
     },
     {
         text: 'Configurações',
         path: '/configuracoes',
         icon: 'fa fa-gear'
-    },
-    {
-        text: 'Check In',
-        path: '/check-in',
-        icon: 'fa-solid fa-money-check'
-    },
-    {
-        text: 'Check Out',
-        path: '/check-out',
-        icon: 'fa-solid fa-check-double'
-    },
-    {
-        text: 'Consumo',
-        path: '/consumo',
-        icon: 'fa-solid fa-cart-shopping'
-    },
-    {
-        text: 'Monitor Reservas',
-        path: '/monitor-reservas',
-        icon: 'folder'
-    },
-    {
-        text: 'Produto',
-        path: '/produto',
-        icon: 'folder'
-    },
-    {
-        text: 'Estoque',
-        path: '/estoque',
-        icon: 'folder'
     }
 ];
