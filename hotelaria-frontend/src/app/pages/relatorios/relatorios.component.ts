c52c9639acd8726c24bd4138f266cdd5943a6750
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2024-01-27 00:18:27 -0300

    Inicio da implementacao para baixar ou visualizar o relatorio, sera abstraido em um componente para emissao de mais de um relatorio simultaneamente

--- hotelaria-frontend/src/app/pages/relatorios/relatorios.component.ts
+++ hotelaria-frontend/src/app/pages/relatorios/relatorios.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {RelatorioService} from "../../shared/services/relatorio.service";
+import notify from "devextreme/ui/notify";
+import _ from "lodash";
 
 @Component({
     selector: 'app-relatorios',
@@ -7,8 +9,14 @@ import {RelatorioService} from "../../shared/services/relatorio.service";
     styleUrls: ['./relatorios.component.scss']
 })
 export class RelatoriosComponent {
+
+    @ViewChild('pdfViewer') pdfViewer: ElementRef;
+
     relatorios: { relatorio: string, opcao: string }[];
     nomeRelatorio: string;
+    fileBlob: Blob;
+    loading: boolean = false;
+    renderVisible: boolean = false;
 
     constructor(private relatorioService: RelatorioService) {
     }
@@ -21,19 +29,40 @@ export class RelatoriosComponent {
     }
 
     gerar() {
+        this.loading = true;
         this.relatorioService.gerarRelatorio(this.nomeRelatorio).subscribe({
             next: resp => {
-                this.downloadRelatorio(resp);
+                this.fileBlob = resp;
+            },
+            complete: () => {
+                this.loading = false;
             }
         });
     }
 
-    downloadRelatorio(blob) {
-        const url = window.URL.createObjectURL(blob);
+    downloadRelatorio() {
+        if (_.isNil(this.fileBlob)) {
+            notify('o arquivo nao esta pronto');
+            return;
+        }
+        const url = window.URL.createObjectURL(this.fileBlob);
         const link = document.createElement('a');
         link.href = url;
         link.download = `${this.nomeRelatorio}.pdf`;
         link.click();
+    }
+
+    renderizarPDF() {
+        this.renderVisible = true;
+
+        const pdfObject = this.pdfViewer.nativeElement;
+        const pdfUrl = URL.createObjectURL(this.fileBlob);
+
+        // Configurar o objeto para exibir o PDF
+        pdfObject.data = pdfUrl;
+        pdfObject.type = 'application/pdf';
+
+        // O conteúdo da tag object será exibido automaticamente
     }
 
     atualizaRelatorio(e: any) {
